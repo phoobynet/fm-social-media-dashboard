fdc62e3424fc7d3a93d6eaefe97cac27fe7765a6
Glenn <phoobynet@users.noreply.github.com>
2023-01-14 14:13:36 +0000

    wip

--- src/components/UserStatCard.tsx
+++ src/components/UserStatCard.tsx
@@ -8,7 +8,15 @@ interface Props {
 export default function UserStatCard({ userStat }: Props) {
   return (
     <div className={styles.userStatCard}>
-      {JSON.stringify(userStat, null, 2)}
+      <div className={styles.topBar}></div>
+      <div className={styles.content}>
+        <div>{userStat.name}</div>
+        <div className={styles.followers}>
+          <div>{userStat.followers}</div>
+          <label>followers</label>
+        </div>
+        <div>{userStat.change} Today</div>
+      </div>
     </div>
   )
 }
